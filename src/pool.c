b807344a34b5c667fd6f1b23a05008650d1a3134
Irvin Lim <irvinlimweiquan@gmail.com>
2018-04-04 19:30:41 +0800

    Remove duplicated includes

--- src/pool.c
+++ src/pool.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <assert.h>
 #include <mpi.h>
 #include "utils/common.h"
 
--- src/poolseq.c
+++ src/poolseq.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/time.h>
-#include <assert.h>
 #include "utils/common.h"
 
 #define PROG "poolseq"
