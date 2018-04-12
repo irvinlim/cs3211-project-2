6a98005ca355a63a07fad829c54d2f4c8b1f2e67
Irvin Lim <irvinlimweiquan@gmail.com>
2018-04-13 04:44:32 +0800

    Update rsync script

--- rsync.sh
+++ rsync.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-rsync -ravz --exclude=".*/" --exclude=".*" -e ssh nscc:~/cs3211-project-2/logs/* logs/
-rsync -ravz -e ssh nscc:~/cs3211-project-2/*.ppm .
-rsync -ravz --exclude=".*/" --exclude=".*" --exclude="logs/*" -e ssh . nscc:~/cs3211-project-2
+echo "Syncing artifacts from remote server..."
+rsync -ravz --include="logs/***" --include="output/***" --include="report/***" --exclude="*" -e ssh nscc:~/cs3211-project-2/ .
+echo
+echo "Syncing source code to remote server..."
+rsync -ravz --exclude=".*/" --exclude=".*" --exclude="logs/*" --exclude="output/*" --exclude="report/*" -e ssh . nscc:~/cs3211-project-2/
+echo
+echo "Done!"
