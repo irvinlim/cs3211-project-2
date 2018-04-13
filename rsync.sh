927b63283ef43b074b1b8c812a2f3ca4ce4b3c96
Irvin Lim <irvinlimweiquan@gmail.com>
2018-04-13 20:38:36 +0800

    Exclude animator from rsync

--- rsync.sh
+++ rsync.sh
@@ -4,6 +4,6 @@ echo "Syncing artifacts from remote server..."
 rsync -ravz --include="logs/***" --include="output/***" --include="report/***" --exclude="*" -e ssh nscc:~/cs3211-project-2/ .
 echo
 echo "Syncing source code to remote server..."
-rsync -ravz --exclude=".*/" --exclude=".*" --exclude="logs/*" --exclude="output/*" --exclude="report/*" -e ssh . nscc:~/cs3211-project-2/
+rsync -ravz --exclude=".*/" --exclude=".*" --exclude="logs/*" --exclude="output/*" --exclude="report/*" --exclude="animator/*" -e ssh . nscc:~/cs3211-project-2/
 echo
 echo "Done!"
