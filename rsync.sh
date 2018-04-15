505f7e0e48412f1c4125549f2b092bf4e2fa64cc
Irvin Lim <irvinlimweiquan@gmail.com>
2018-04-15 17:06:03 +0800

    Update PBS and rsync for frames

--- rsync.sh
+++ rsync.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Syncing artifacts from remote server..."
-rsync -ravz --include="logs/***" --include="output/***" --include="report/***" --exclude="*" -e ssh nscc:~/cs3211-project-2/ .
+rsync -ravz --include="logs/***" --include="output/***" --include="report/***" --include="frames/***" --exclude="*" -e ssh nscc:~/cs3211-project-2/ .
 echo
 echo "Syncing source code to remote server..."
-rsync -ravz --exclude=".*/" --exclude=".*" --exclude="logs/*" --exclude="output/*" --exclude="report/*" --exclude="animator/*" -e ssh . nscc:~/cs3211-project-2/
+rsync -ravz --exclude=".*/" --exclude=".*" --exclude="logs/*" --exclude="output/*" --exclude="report/*" --exclude="frames/*" --exclude="animator/*" -e ssh . nscc:~/cs3211-project-2/
 echo
 echo "Done!"
