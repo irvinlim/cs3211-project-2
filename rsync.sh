72bf6ab3fd5258a01dde5183b592ff63ca4c7ffd
Irvin Lim <irvinlimweiquan@gmail.com>
2018-04-15 17:47:24 +0800

    Change output directory on NSCC to data/

--- rsync.sh
+++ rsync.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Syncing artifacts from remote server..."
-rsync -ravz --include="logs/***" --include="output/***" --include="report/***" --include="frames/***" --exclude="*" -e ssh nscc:~/cs3211-project-2/ .
+rsync -ravz --include="data/***" --exclude="*" -e ssh nscc:~/cs3211-project-2/ .
 echo
 echo "Syncing source code to remote server..."
-rsync -ravz --exclude=".*/" --exclude=".*" --exclude="logs/*" --exclude="output/*" --exclude="report/*" --exclude="frames/*" --exclude="animator/*" -e ssh . nscc:~/cs3211-project-2/
+rsync -ravz --exclude=".*/" --exclude=".*" --exclude="data/***" --exclude="animator/***" --exclude="docs/***" -e ssh . nscc:~/cs3211-project-2/
 echo
 echo "Done!"
